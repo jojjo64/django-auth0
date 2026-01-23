7c0a3034ef089a189cb78d2470d1a8b67b321cba
jojjo64 <rattan_stupse4d@icloud.com>
2026-01-23 21:35:07 +0100

    correct typo

--- src/django_auth0/urls.py
+++ src/django_auth0/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from django_auth0.views import auth0_login, auth0_callback, auth0_logout
 
-app_name = "azure_auth"
+app_name = "auth0_auth"
 urlpatterns = [
     path("login", auth0_login, name="login"),
     path("logout", auth0_logout, name="logout"),
