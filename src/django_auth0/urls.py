efc4737219b9db5cea5f6baa3c537800d647c046
jojjo64 <rattan_stupse4d@icloud.com>
2026-01-30 17:31:49 +0100

    swapped sequence of callback and logout

--- src/django_auth0/urls.py
+++ src/django_auth0/urls.py
@@ -5,6 +5,6 @@ from django_auth0.views import auth0_login, auth0_callback, auth0_logout
 app_name = "auth0_auth"
 urlpatterns = [
     path("login", auth0_login, name="login"),
-    path("logout", auth0_logout, name="logout"),
     path("callback", auth0_callback, name="callback"),
+    path("logout", auth0_logout, name="logout"),
 ]
